e394d906914ec5d0fbb8fa52a23d36fb82564996
Júnior Nunes <denisonprobable13@gmail.com>
2022-04-22 18:08:29 -0300

    jest installed and configured

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    'jest-styled-components',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+};
