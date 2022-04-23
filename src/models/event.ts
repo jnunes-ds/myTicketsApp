850a4278bd5b5d0467c3042176d16a930fa0fd13
Júnior Nunes <denisonprobable13@gmail.com>
2022-04-23 10:10:51 -0300

    event interface created

---
+++ src/models/event.ts
@@ -0,0 +1,31 @@
+type weekDayType =
+	'Mon'
+	| 'Tue'
+	| 'Wed'
+	| 'Thu'
+	| 'Fri'
+	| 'Sat'
+	| 'Sun';
+
+type N = `${number}${number}`;
+type houtType = `${N}:${N}`;
+
+interface IDuration {
+	starts_at: houtType;
+	ends_at: houtType;
+}
+
+interface IDaysLong {
+	week_day: weekDayType;
+	date: Date;
+	duration: IDuration;
+}
+
+export interface IEvent {
+	id: string;
+	name: string;
+	company_id: string;
+	company_name: string;
+	description: string;
+	days_long: IDaysLong[];
+};
