d644335925d4d386e00c74747f051eb8d720a062
Júnior Nunes <denisonprobable13@gmail.com>
2022-04-23 11:55:04 -0300

    light theme created

---
+++ src/global/themes/light.ts
@@ -0,0 +1,44 @@
+export default {
+	colors: {
+		background: {
+			default: '#F2F2F2',
+			primary: '#FFFFFF',
+			secondary: '#FFFFFF'
+		},
+		fontAndIcon: {
+			primary: '#000000',
+			secondary: '#7F7F7F',
+			tertiary: '#BFBFBF',
+			white: '#FFFFFF',
+			red: '#FF3B30',
+			blue: '#007AFF',
+			yellow: '#FFCC00',
+			green: '#34C759',
+		},
+		brand: {
+			primary: '#1EC7FA',
+			secondary: '#1E6FF1'
+		},
+		fill: {
+			primary: '#D8D8D8',
+			secondary: '#BFBFBF'
+		}
+	},
+	fonts: {
+		family: {
+			primary: 'Roboto-Medium',
+			primaryBold: 'Roboto-Bold',
+			secondary: 'Lato-Regular',
+			secondaryBold: 'Lato-Bold'
+		},
+		sizes: {
+			tinny: 11,
+			small1: 13,
+			small2: 15,
+			reguçar: 17,
+			medium: 22,
+			big: 28,
+			huge: 34
+		}
+	}
+}
