81910f42e997c09ccd40cda006fba463b49d2d20
Júnior Nunes <denisonprobable13@gmail.com>
2022-04-23 11:58:31 -0300

    dark theme created

---
+++ src/global/themes/dark.ts
@@ -0,0 +1,44 @@
+export default {
+	colors: {
+		background: {
+			default: '#0C0C0C',
+			primary: '#191919',
+			secondary: '#000000'
+		},
+		fontAndIcon: {
+			primary: '##E5E5E5',
+			secondary: '#7F7F7F',
+			tertiary: '#3F3F3F',
+			white: '#FFFFFF',
+			red: '#FF453A',
+			blue: '#0A84FF',
+			yellow: '#FFD60A',
+			green: '#32D74B',
+		},
+		brand: {
+			primary: '#1EC7FA',
+			secondary: '#1E6FF1'
+		},
+		fill: {
+			primary: '#262626',
+			secondary: '#3F3F3F'
+		}
+	},
+	fonts: {
+		family: {
+			primary: 'Roboto-Medium',
+			primaryBold: 'Roboto-Bold',
+			secondary: 'Lato-Regular',
+			secondaryBold: 'Lato-Bold'
+		},
+		sizes: {
+			tinny: 11,
+			small1: 13,
+			small2: 15,
+			reguçar: 17,
+			medium: 22,
+			big: 28,
+			huge: 34
+		}
+	}
+}
